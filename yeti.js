c665b9c6d960296634aa9808c1777bfe476548ba
Ellen <ellen.cornelius@bonus.ly>
2020-04-13 18:20:22 -0600

    Add yeti class

---
+++ yeti.js
@@ -0,0 +1,31 @@
+class Yeti {
+  constructor(name, home, weight){
+    this.name = name;
+    this.home = home;
+    this.weight = weight;
+    this.reindeer_eaten = ["Rudolph"]
+  }
+
+  run_away() {
+    this.weight -= 10;
+    console.log(`We'll have to outwit the ${name} with our superior intelligence.`)
+  }
+
+  number_of_reindeer_eaten() {
+    this.reindeers_eaten.count
+  }
+
+  isHungry {
+    if (this.weight > 300) {
+      return false
+    } else {
+      return true
+
+  }
+}
+
+yeti = new Yeti("Bumble", "Colorado")
+yeti.number_of_reindeer_eaten()
+yeti.isHungry()
+yeti.run_away()
+yeti.bounce()
